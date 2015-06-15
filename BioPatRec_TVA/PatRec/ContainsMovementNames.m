37e4674e5ebf6fe997e1605911845e05bb0a14fe
biopatrec <morder.mbk@gmail.com>
2015-06-15 16:36:57 +0200

    Creation of Data Repository branch

--- BioPatRec_TVA/PatRec/ContainsMovementNames.m
+++
@@ -1,8 +0,0 @@
-function contains = ContainsMovementNames(movements,movementNames)
-    contains = 0;
-    for i = 1:length(movements)
-       if strfind(movementNames{1},movements(i).name{1})
-           contains = 1;
-       end
-    end
-end
