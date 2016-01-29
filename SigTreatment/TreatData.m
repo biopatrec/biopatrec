4bfade3e274278396a95917d7cff29e5fe36bca6
Julian Maier <julian.maier@mail.com>
2016-01-30 00:27:25 +0100

    Update TreatData.m

--- SigTreatment/TreatData.m
+++ SigTreatment/TreatData.m
@@ -1,14 +1,14 @@
 % ---------------------------- Copyright Notice ---------------------------
-% This file is part of BioPatRec © which is open and free software under 
+% This file is part of BioPatRec Â© which is open and free software under 
 % the GNU Lesser General Public License (LGPL). See the file "LICENSE" for 
 % the full license governing this code and copyrights.
 %
 % BioPatRec was initially developed by Max J. Ortiz C. at Integrum AB and 
-% Chalmers University of Technology. All authors’ contributions must be kept
+% Chalmers University of Technology. All authorsâ€™ contributions must be kept
 % acknowledged below in the section "Updates % Contributors". 
 %
 % Would you like to contribute to science and sum efforts to improve 
-% amputees’ quality of life? Join this project! or, send your comments to:
+% amputeesâ€™ quality of life? Join this project! or, send your comments to:
 % maxo@chalmers.se.
 %
 % The entire copyright notice must be kept in this or any source file 
@@ -51,7 +51,7 @@ function sigTreated = TreatData(handles, sigTreated)
      
     allSigSeperaAlg             = get(handles.pm_SignalSeparation,'String');
     selSigSeperaAlg             = allSigSeperaAlg(get(handles.pm_SignalSeparation,'Value'));
-    sigTreated.sigSeperation.Alg= selSigSeperaAlg;
+    sigTreated.sigSeparation.Alg= selSigSeperaAlg;
     
     %% Filters
     set(handles.t_msg,'String','Applying filters...');
