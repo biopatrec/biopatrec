2bf4381b6edf5ca0a5274d0e864eba9b229c81c5
Julian Maier <julian.maier@mail.com>
2016-01-30 00:45:32 +0100

    Update ICAPreprocess.m

--- SigTreatment/ICA/ICAPreprocess.m
+++ SigTreatment/ICA/ICAPreprocess.m
@@ -1,14 +1,14 @@
 % ---------------------------- Copyright Notice ---------------------------
-% This file is part of BioPatRec © which is open and free software under
+% This file is part of BioPatRec Â© which is open and free software under
 % the GNU Lesser General Public License (LGPL). See the file "LICENSE" for
 % the full license governing this code and copyrights.
 %
 % BioPatRec was initially developed by Max J. Ortiz C. at Integrum AB and
-% Chalmers University of Technology. All authors’ contributions must be kept
+% Chalmers University of Technology. All authorsâ€™ contributions must be kept
 % acknowledged below in the section "Updates % Contributors".
 %
 % Would you like to contribute to science and sum efforts to improve
-% amputees’ quality of life? Join this project! or, send your comments to:
+% amputeesâ€™ quality of life? Join this project! or, send your comments to:
 % maxo@chalmers.se.
 %
 % The entire copyright notice must be kept in this or any source file
@@ -29,7 +29,7 @@ function [trData,vData,tData]=ICAPreprocess(sigTreated,trData,vData,tData)
 
       
           
-      W=sigTreated.sigSeperation.ICAUnmixMat;
+      W=sigTreated.sigSeparation.ICAUnmixMat;
        
  % Projecting ICA Unmixing Matrix on Training Sets
 % 
